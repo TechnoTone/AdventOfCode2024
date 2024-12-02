941ac0d3077586c7a9f669374a4f234c18a52332
Tony Hunt <thunt@findmypast.com>
2024-12-02 12:36:36 +0000

    Day 1 refactor

--- JS/src/day01.js
+++ JS/src/day01.js
@@ -1,17 +1,9 @@
 module.exports.part1 = (input) => {
-  const [listA, listB] = input.reduce(
-    (acc, line) => {
-      const [a, b] = line.split(/\s+/).map(Number);
-      acc[0].push(a);
-      acc[1].push(b);
-      return acc;
-    },
-    [[], []]
-  );
-
+  const [listA, listB] = parseInput(input);
   listA.sort();
   listB.sort();
   let distance = 0;
+
   for (let i = 0; i < listA.length; i++) {
     distance += Math.abs(listA[i] - listB[i]);
   }
@@ -20,7 +12,25 @@ module.exports.part1 = (input) => {
 };
 
 module.exports.part2 = (input) => {
-  const [listA, listB] = input.reduce(
+  const [listA, listB] = parseInput(input);
+  const scores = {};
+
+  listA.forEach((b) => {
+    if (!scores[b]) {
+      scores[b] = 1;
+    } else {
+      scores[b]++;
+    }
+  });
+
+  return listB.reduce((acc, a) => {
+    if (scores[a]) return acc + scores[a] * a;
+    return acc;
+  }, 0);
+};
+
+function parseInput(input) {
+  return input.reduce(
     (acc, line) => {
       const [a, b] = line.split(/\s+/).map(Number);
       acc[0].push(a);
@@ -29,19 +39,4 @@ module.exports.part2 = (input) => {
     },
     [[], []]
   );
-
-  const scores = {};
-
-  listB.forEach((b) => {
-    if (!scores[b]) {
-      scores[b] = 1;
-    } else {
-      scores[b]++;
-    }
-  });
-
-  return listA.reduce((acc, a) => {
-    if (scores[a]) return acc + scores[a] * a;
-    return acc;
-  }, 0);
-};
+}
